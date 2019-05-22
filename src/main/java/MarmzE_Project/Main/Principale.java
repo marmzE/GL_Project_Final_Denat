75611f9116faf336eaf4e43340715276117c2832
marmzE <martin.denat@outlook.fr>
2019-05-22 22:36:17 +0200

    ajout de la méthode lireTexte

--- src/main/java/MarmzE_Project/Main/Principale.java
+++ src/main/java/MarmzE_Project/Main/Principale.java
@@ -1,8 +1,41 @@
 package MarmzE_Project.Main;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Principale {
 
 	public static void main(String [] args){
 		//a faire
 		
 	}
+	
+	
+	public static void lireText(){
+		try{
+			InputStream flux=new FileInputStream("test.txt"); 
+			InputStreamReader lecture=new InputStreamReader(flux);
+			BufferedReader buff=new BufferedReader(lecture);
+			String ligne;
+			int nbLigne =1;
+			while ((ligne=buff.readLine())!=null){
+				gestionLigne(ligne,nbLigne);
+			}
+			buff.close(); 
+			}		
+			catch (Exception e){
+			System.out.println(e.toString());
+			}
+	}
+
+
+	private static void gestionLigne(String ligne, int nbLigne) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
 }
